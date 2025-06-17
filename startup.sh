3a09ee35d2133e0ecdec972927328c42d9f468c9
Saoussen Attia <attia.saoussen@gmail.com>
2025-06-17 12:38:22 +0200

    modification déploiement startup

--- startup.sh
+++ startup.sh
@@ -1,4 +1,15 @@
+#!/bin/bash
 
+echo "Début du script startup.sh"
 
-# Lancer l'application avec Gunicorn + Uvicorn worker
+if [ -d "antenv" ]; then
+    source antenv/bin/activate
+    echo "Environnement activé"
+else
+    echo "Environnement virtuel antenv non trouvé"
+fi
+
+pip install --upgrade pip
+pip install -r requirements.txt
+
 exec gunicorn -k uvicorn.workers.UvicornWorker app:app --bind=0.0.0.0:8000 --timeout 120
