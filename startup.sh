99ccf468657e364b037e52ac757af3bd9755f7cc
Saoussen Attia <attia.saoussen@gmail.com>
2025-06-17 12:21:54 +0200

    modification déploiement

--- startup.sh
+++ startup.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
 
-# Activer l'environnement virtuel s'il existe
-if [ -d "antenv" ]; then
-    source antenv/bin/activate
-fi
-
-# Installer les dépendances (en cas de build partiel)
-pip install --upgrade pip
-pip install -r requirements.txt
 
 # Lancer l'application avec Gunicorn + Uvicorn worker
-exec gunicorn -k uvicorn.workers.UvicornWorker main:app --bind=0.0.0.0:8000 --timeout 120
+exec gunicorn -k uvicorn.workers.UvicornWorker app:app --bind=0.0.0.0:8000 --timeout 120
