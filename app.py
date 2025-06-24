d422edba263024a42ce9f5775e473e5a543d3fbb
ATTIA <116119712+ASaoussen@users.noreply.github.com>
2025-06-24 20:48:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -129,5 +129,5 @@ def predict():
     })
 
 # --- Lancement local ---
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+#if __name__ == "__main__":
+app.run(host="0.0.0.0", port=8000)
